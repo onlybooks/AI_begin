48bfaafb9db3d698715ded0a6c70d4ee4ab53bfc
charlesma <charlesma73@gmail.com>
2020-03-14 16:15:32 +0900

    pandas ix 사용 수정
    pandas 버전 0.20.0 이후 ix 사용이 종료되어 최근 버전에서는 오류를 발생시키기 때문에 ix를 사용하지 않고 실행하도록 수정
    [참조] https://pandas.pydata.org/pandas-docs/version/0.25.0/reference/api/pandas.Series.ix.html

--- chapter03/mushroom.py
+++ chapter03/mushroom.py
@@ -1,34 +1,31 @@
 # -*- coding: utf-8 -*-
 
-#pandas 모듈 추가
+# pandas 모듈 추가
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
 # 데이타 셋 로드 (1)
 mushroom = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data',
-        header = None)
-
+                       header=None)
 
 # 문자를 수치로 치환 (2)
-X = []
+x = []
 y = []
 
 for row_index, row in mushroom.iterrows():
-    y.append(row.ix[0])
+    y.append(row[0])
     row_X = []
-    for v in row.ix[1:]:
-        row_X.append(ord(v)) # (3)
-    X.append(row_X)
-    
+    for v in row[1:]:
+        row_X.append(ord(v))  # (3)
+    x.append(row_X)
 
 # 학습 데이터와 테스트 데이터 나누기 (4)
-x_train, x_test, y_train, y_test = train_test_split(X, y)
-
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 # 학습 및 테스트 (5)
 svc = SVC()
 svc.fit(x_train, y_train)
 
-print('훈련 점수 : {: .3f}'.format(svc.score (x_train, y_train)))
-print('테스트 점수 : {: .3f}'.format(svc.score (X_test, y_test)))
+print('훈련 점수 : {:.3f}'.format(svc.score(x_train, y_train)))
+print('테스트 점수 : {:.3f}'.format(svc.score(x_test, y_test)))
